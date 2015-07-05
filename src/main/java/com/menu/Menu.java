c2f068e3544eace0ef3d4a89ec47629cdf88af10
wuzupko <wuzupko@gmail.com>
2015-07-05 17:13:10 +0300

    Super Menu

--- src/main/java/com/menu/Menu.java
+++ src/main/java/com/menu/Menu.java
@@ -1,6 +1,9 @@
 package com.menu;
 import org.apache.logging.log4j.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.ArrayList;
 
 /**
@@ -74,12 +77,12 @@ public class Menu{
 
 
 
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
-//        EntityManager em = emf.createEntityManager();
-//
-//        em.close();
-//
-//        emf.close(); //close at application end
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+
+        emf.close(); //close at application end
 
     }
 
